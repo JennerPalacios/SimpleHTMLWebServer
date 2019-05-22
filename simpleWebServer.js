452c05af9c04a555b81fc2d91770a84d9908dc2e
Jenner Palacios <japp_17@hotmail.com>
2019-05-22 13:47:12 -0700

    Adding Colors

--- simpleWebServer.js
+++ simpleWebServer.js
@@ -4,16 +4,38 @@ const fs=require("fs");
 const logRequests="yes";
 const logRequestor="yes";
 
+// CONSOLE COLORS
+const colors={
+	"Reset": "\x1b[0m",
+	"FgBlack": "\x1b[30m",
+	"FgRed": "\x1b[31m",
+	"FgGreen": "\x1b[32m",
+	"FgYellow": "\x1b[33m",
+	"FgBlue": "\x1b[34m",
+	"FgMagenta": "\x1b[35m",
+	"FgCyan": "\x1b[36m",
+	"FgWhite": "\x1b[37m",
+	"BgBlack": "\x1b[40m",
+	"BgRed": "\x1b[41m",
+	"BgGreen": "\x1b[42m",
+	"BgYellow": "\x1b[43m",
+	"BgBlue": "\x1b[44m",
+	"BgMagenta": "\x1b[45m",
+	"BgCyan": "\x1b[46m",
+	"BgWhite": "\x1b[47m"
+}
+
 // FUNCTION: TIME STAMP
 function timeStamp(){
 	let CurrTime=new Date();
 	let mo=CurrTime.getMonth()+1;if(mo<10){mo="0"+mo;}let da=CurrTime.getDate();if(da<10){da="0"+da;}let yr=CurrTime.getFullYear();
 	let hr=CurrTime.getHours();if(hr<10){hr="0"+hr;}let min=CurrTime.getMinutes();if(min<10){min="0"+min;}let sec=CurrTime.getSeconds();if(sec<10){sec="0"+sec;}
-	return "["+yr+"/"+mo+"/"+da+" @ "+hr+":"+min+":"+sec+"]"
+	return colors.FgBlue+yr+"/"+mo+"/"+da+" @ "+hr+":"+min+":"+sec+colors.Reset+" |"
 }
 
+
 // CONSOLE WHEN LAUNCHED
-console.info(timeStamp()+" -- [localhost] WebServer is now READY --");
+console.info(timeStamp()+" -- ["+colors.FgMagenta+"localhost"+colors.Reset+"] WebServer is now "+colors.FgGreen+"READY"+colors.Reset+" --");
 
 // VARIABLES AND ARRAYS
 var requestor=""
@@ -25,7 +47,7 @@ const MSG_NOTFOUND="<br /><br /><center><h1>[<span style='color:#F00'>404</span>
 // CREATE HTTP SERVER
 http.createServer(function (requested, response) {
 	if(requested.url==="/"){
-		console.info(timeStamp()+" Attempting to access file:  index.html")
+		console.info(timeStamp()+" Attempting to access file: "+colors.FgMagenta+"index.html")
 		requested.url="/index.html"
 	}
 	let extension=requested.url.split("."); extension=extension[1];
@@ -34,7 +56,7 @@ http.createServer(function (requested, response) {
 		if(requested.headers['user-agent']!==requestor){
 			requestor=requested.headers['user-agent'];
 			if(logRequestor==="yes"){
-				console.info(timeStamp()+" Incoming Request from: "+requestor)
+				console.info(timeStamp()+" Incoming Request from: "+colors.FgCyan+requestor)
 			}
 		}
 	}
@@ -44,12 +66,12 @@ http.createServer(function (requested, response) {
 			if (err) {
 				response.writeHead(404, {'Content-Type': 'text/html'});
 				if(logRequests==="yes"){
-					console.info(timeStamp()+" File Requested: "+requested.url.slice(1)+"   <- - -   NOT FOUND!")
+					console.info(timeStamp()+" File Requested: "+colors.FgMagenta+requested.url.slice(1)+colors.Reset+"   <- - -   "+colors.FgRed+"NOT FOUND!")
 				}
 				return response.end(MSG_NOTFOUND);
 			}
 			if(logRequests==="yes"){
-				console.info(timeStamp()+" File Requested: "+requested.url.slice(1))
+				console.info(timeStamp()+" File Requested: "+colors.FgMagenta+requested.url.slice(1))
 			}
 			response.writeHead(200, {'Content-Type': fileType});
 			response.write(data);
@@ -58,8 +80,8 @@ http.createServer(function (requested, response) {
 	}
 	else {
 		response.writeHead(404, {'Content-Type': 'text/html'});
-		console.info(timeStamp()+" File Requested: "+requested.url.slice(1)+"   <- - -   NOT FOUND!")
-		console.info(timeStamp()+" [ERROR] INVALID FILE - FILETYPE NOT ACCEPTED!");
+		console.info(timeStamp()+" File Requested: "+colors.FgMagenta+requested.url.slice(1)+colors.Reset+"   <- - -   "+colors.FgRed+"NOT FOUND!");
+		console.info(timeStamp()+" "+colors.FgWhite+colors.BgRed+" ERROR "+colors.Reset+" INVALID FILE - FILETYPE NOT ACCEPTED!");
 		return response.end(MSG_NOTFOUND);
 	}
 }).listen(80);
